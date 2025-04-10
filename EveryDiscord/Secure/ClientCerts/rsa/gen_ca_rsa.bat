4178965d5161aa23098a4fe8bfda3c5f685ac44b
gaouser <87039784+gaouservbf@users.noreply.github.com>
2025-04-11 00:19:33 +0300

    a
    a

--- EveryDiscord/Secure/ClientCerts/rsa/gen_ca_rsa.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-openssl genrsa -out ca_rsa.key 4096
-openssl req -new -x509 -days 3650 -key ca_rsa.key -out ca_rsa.pem
--- EveryDiscord/Secure/drag_to_view_pfx.bat
+++
@@ -1,2 +0,0 @@
-@openssl pkcs12 -in %1 -nodes -passin pass:"" | openssl x509 -text
-@pause
